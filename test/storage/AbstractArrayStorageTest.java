88a019379a3e2622de3dc9fe38c8d79c59fffea5
bedroom <borzsonte@gmail.com>
2020-11-19 06:12:49 +0300

    AbstractArrayStorageTest add

---
+++ test/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,68 @@
+package storage;
+
+import exсeption.NotExistStorageException;
+import model.Resume;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class AbstractArrayStorageTest {
+
+    private Storage storage;
+    public AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
+
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+        ArrayStorage subArray = (ArrayStorage) storage;
+        System.out.println(subArray[0]);
+
+    }
+
+    @Test
+    public void size() {
+        Assert.assertEquals(3, storage.size());
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() throws Exception {
+        storage.get("dummy");
+    }
+}
---
+++ test/storage/ArrayStorageTest.java
@@ -0,0 +1,9 @@
+package storage;
+
+import static org.junit.Assert.*;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+}
