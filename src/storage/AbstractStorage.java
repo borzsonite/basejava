d395915f8f9709a32a5a1d3b1321d2234290e064
Sergey Rozhkov <borzsonite@gmail.com>
2020-12-06 09:07:40 +0300

    HW5_part1 done

--- src/storage/AbstractStorage.java
+++ src/storage/AbstractStorage.java
@@ -4,27 +4,20 @@ import exсeption.ExistStorageException;
 import exсeption.NotExistStorageException;
 import model.Resume;
 
-//Задача: перенести в класс код который будет общим для ArrayStorage, SortedArrayStorage, ListStorage, MapStorage. Общим будет код который проверяет в методах наличие/отуствие резюме в хранилище. Чтобы это сделать нужно зная uuid резюме пройтись по хранилищу на предмет наличия там резюме с соответствующим uuid. Возращаться будет либо null либо позиция резюме в хранилицще в виде индекса (лист, массив) или ключа (мапа).
-
 public abstract class AbstractStorage implements Storage {
 
     protected abstract Object getIndex(String uuid);
-
     protected abstract void proceedUpdate(Object resumePosition, Resume resume);
-
     protected abstract void proceedSave(Object resumePosition, Resume resume);
-
     protected abstract Resume proceedGet(Object resumePosition);
-
     protected abstract void proceedDelete(Object resumePosition);
 
     public void update(Resume resume) {
         Object resumePosition = getIndex(resume.getUuid());
-        if (resumePosition == Integer.valueOf(-1)) {
-            throw new NotExistStorageException(resume.getUuid());
+        if(isExist(resumePosition, resume.getUuid())) {
+            proceedUpdate(resumePosition, resume);
+            System.out.println("Resume " + resume.getUuid() + " updated.");
         }
-        proceedUpdate(resumePosition, resume);
-        System.out.println("Resume " + resume.getUuid() + " updated.");
     }
 
     public void save(Resume resume) {
@@ -38,19 +31,24 @@ public abstract class AbstractStorage implements Storage {
 
     public Resume get(String uuid) {
         Object resumePosition = getIndex(uuid);
-        if (resumePosition == Integer.valueOf(-1)) {
-            throw new NotExistStorageException(uuid);
+        if(isExist(resumePosition, uuid)) {
+            return proceedGet(resumePosition);
         }
-        return proceedGet(resumePosition);
+        return null;
     }
 
     public void delete(String uuid) {
         Object resumePosition = getIndex(uuid);
-        if (resumePosition == Integer.valueOf(-1)) {
-            throw new NotExistStorageException(uuid);
+        if(isExist(resumePosition, uuid)) {
+            proceedDelete(resumePosition);
+            System.out.println("Resume " + uuid + " deleted");
         }
-        proceedDelete(resumePosition);
-        System.out.println("Resume " + uuid + " deleted");
     }
 
+    private boolean isExist(Object resumePosition, String uuid) {
+        if(resumePosition == Integer.valueOf(-1)) {
+            throw new NotExistStorageException(uuid);
+        }
+        return true;
+    }
 }
