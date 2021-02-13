38aa5a11ee5a3f49dc60da309614df54e8324d8a
Sergey Rozhkov <borzsonite@gmail.com>
2021-02-13 21:04:47 +0300

    tests fixed

--- src/model/Organization.java
+++ src/model/Organization.java
@@ -1,7 +1,10 @@
 package model;
 
+import util.LocalDateAdapter;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.Month;
@@ -56,8 +59,11 @@ public class Organization implements Serializable {
         return link + "" + position;
     }
 
+    @XmlAccessorType(XmlAccessType.FIELD)
     public static class Position implements Serializable {
+        @XmlJavaTypeAdapter(LocalDateAdapter.class)
         private LocalDate startDate;
+        @XmlJavaTypeAdapter(LocalDateAdapter.class)
         private LocalDate endDate;
         private String title;
         private String description;
