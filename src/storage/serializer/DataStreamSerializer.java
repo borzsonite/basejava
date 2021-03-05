06ef179c0c7ecdc655ffdf594de59e6610f3a191
Sergey Rozhkov <borzsonite@gmail.com>
2021-03-05 13:01:58 +0300

    HW10 fix5

--- src/storage/serializer/DataStreamSerializer.java
+++ src/storage/serializer/DataStreamSerializer.java
@@ -20,25 +20,16 @@ public class DataStreamSerializer implements StreamSerializer {
             dos.writeUTF(r.getFullName());
             Map<ContactType, String> contacts = r.getContacts();
 
-            writeWithException(contacts.entrySet(), collectionElement -> {
+            writeWithException(dos, contacts.entrySet(), collectionElement -> {
                 dos.writeUTF(String.valueOf(collectionElement.getKey()));
                 dos.writeUTF(collectionElement.getValue());
-            }, dos);
+            });
 
             Map<SectionType, AbstractSection> sections = r.getAllSections();
-           // dos.writeInt(sections.size());
-//            for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
-//                dos.writeUTF(entry.getKey().name());
-//                abstractSectionWrite(entry.getKey(), dos, r);
-//            }
-            // вместо цикла вызывается метод в который передается entrySet map'ы, лямбда, dos
-            writeWithException(sections.entrySet(), new Writable<Map.Entry<SectionType, AbstractSection>>() {
-                @Override
-                public void accept(Map.Entry<SectionType, AbstractSection> collectionElement) throws IOException {
-                    abstractSectionWrite(collectionElement, dos);
-                }
-            }, dos);
-
+            writeWithException(dos, sections.entrySet(), collectionElement -> {
+                dos.writeUTF(String.valueOf(collectionElement.getKey()));
+                abstractSectionWrite(collectionElement, dos);
+            });
         }
     }
 
@@ -66,39 +57,32 @@ public class DataStreamSerializer implements StreamSerializer {
         switch (collection.getKey()) {
             case PERSONAL:
             case OBJECTIVE:
-                //dos.writeUTF(((TextSection) r.getSection(sectionType)).getContent());
                 dos.writeUTF(String.valueOf(collection.getValue()));
                 break;
 
             case ACHIEVEMENT:
             case QUALIFICATION:
-                ListSection listSection = (ListSection) collection.getValue();
-                List<String> listSectionsItems = listSection.getItems();
-                dos.writeInt(listSectionsItems.size());
-                for (String elem : listSectionsItems) {
-                    dos.writeUTF(elem);
-                }
+                List<String> listSectionsItems = ((ListSection) collection.getValue()).getItems();
+                writeWithException(dos, listSectionsItems, dos::writeUTF);
                 break;
 
             case EXPERIENCE:
             case EDUCATION:
                 OrganizationSection organizationSection = (OrganizationSection) collection.getValue();
                 List<Organization> organizations = organizationSection.getOrganisations();
-                dos.writeInt(organizations.size());
-                for (Organization organization : organizations) {
-                    Link link = organization.getLink();
+                writeWithException(dos, organizations, collectionElement -> {
+                    Link link = collectionElement.getLink();
                     String url = link.getUrl();
                     dos.writeUTF(link.getName());
                     dos.writeUTF(url != null ? url : "");
-                    dos.writeInt(organization.getPosition().size());
 
-                    for (Organization.Position position : organization.getPosition()) {
-                        writeDate(dos, position.getStartDate());
-                        writeDate(dos, position.getEndDate());
-                        dos.writeUTF(position.getTitle());
-                        dos.writeUTF(position.getDescription() != null ? position.getDescription() : "");
-                    }
-                }
+                    writeWithException(dos, collectionElement.getPosition(), collectionElement1 -> {
+                        writeDate(dos, collectionElement1.getStartDate());
+                        writeDate(dos, collectionElement1.getEndDate());
+                        dos.writeUTF(collectionElement1.getTitle());
+                        dos.writeUTF(collectionElement1.getDescription() != null ? collectionElement1.getDescription() : "");
+                    });
+                });
         }
     }
 
@@ -151,10 +135,10 @@ public class DataStreamSerializer implements StreamSerializer {
         return LocalDate.of(dis.readInt(), Month.of(dis.readInt()), 1);
     }
 
-    protected <T> void writeWithException(Collection<T> collection, Writable<T> action, DataOutputStream dos) throws IOException {
+    protected <T> void writeWithException(DataOutputStream dos, Collection<T> collection, Writable<T> action) throws IOException {
         int size = collection.size();
         dos.writeInt(size);
-        for(T elem: collection) {
+        for (T elem : collection) {
             action.accept(elem);
         }
     }
