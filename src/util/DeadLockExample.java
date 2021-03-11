ebe73abf2cfef888c6bf0f349d163052ff2d567f
Sergey Rozhkov <borzsonite@gmail.com>
2021-03-11 13:12:45 +0300

    DeadLockExample add

---
+++ src/util/DeadLockExample.java
@@ -0,0 +1,65 @@
+package util;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DeadLockExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        DeadLockRunner dlr = new DeadLockRunner();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                dlr.processThis();
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                dlr.processThat();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(dlr.getCounter());
+    }
+
+}
+
+    class DeadLockRunner {
+        private int counter;
+        private Lock lockA = new ReentrantLock();
+        private Lock lockB = new ReentrantLock();
+
+        public void processThis() {
+            lockA.lock();
+            lockB.lock();
+            for(int i=0; i<10000; i++) {
+                counter++;
+            }
+            lockA.unlock();
+            lockB.unlock();
+        }
+
+        public void processThat() {
+            lockB.lock();
+            lockA.lock();
+            for(int i=0; i<10000; i++) {
+                counter++;
+            }
+            lockA.unlock();
+            lockB.unlock();
+        }
+
+        public int getCounter() {
+            return counter;
+        }
+    }
+
