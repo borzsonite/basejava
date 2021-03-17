f4a5177887cf2aeaf2162ad9588fff065181c9ad
Sergey Rozhkov <borzsonite@gmail.com>
2021-03-18 00:11:08 +0300

    HW12 done

--- src/MainStreams.java
+++ src/MainStreams.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntSupplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MainStreams {
     public static void main(String[] args) {
-        int[] array = {1,2,3,3,2,3};
-        System.out.println(minValue(array));
+
+        System.out.println(minValue(new int[] {1,2,3,3,2,3}));
+        System.out.println(oddOrEven(Arrays.asList(1,2,3,3,2,3,4,5,6,1)));
     }
 
     static int minValue(int[] values) {
        return Arrays.stream(values).distinct().sorted().reduce(0, (acc, x) -> (10 * acc + x));
     }
 
+
     static List<Integer> oddOrEven(List<Integer> integers) {
-        integers.stream()
-        return new ArrayList<>();
+
+        Integer sum = integers.stream().reduce(0, Integer::sum);
+        return sum%2==0?integers.stream()
+                .filter(x->x%2 ==0)
+                .collect(Collectors.toList()):integers.stream()
+                .filter(x->x%2 !=0)
+                .collect(Collectors.toList());
+
     }
 }
