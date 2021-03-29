6666a3eb7ceac713ab53df155ee7c6b86793bb51
Sergey <borzsonite@gmail.com>
2021-03-29 21:14:58 +0300

    Config issue

--- test/storage/SqlStorageTest.java
+++ test/storage/SqlStorageTest.java
@@ -1,9 +1,10 @@
 package storage;
 
-import com.sun.deploy.config.Config;
-import db_config.Dbconfig;
+import com.sun.deploy.config.Config; // не видит Config из проекта
+import db_config.Dbconfig; // этот видит но не видит в нем методов,
 
 
 public class SqlStorageTest {
     super(Config.get().getStorageDir());
+    super(Dbconfig.get().getStorageDir()); // хотя в AbstractStorageTest такой вызов срабатывает
 }
